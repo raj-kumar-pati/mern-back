d92bb93695af3766e00e788e4c05120758ec516b
raj-kumar-pati <rajkumarpati6969@gmail.com>
2024-10-29 23:45:10 +0530

    port seating

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
-export const PORT = 5555 || process.env.PORT;
+export const PORT = process.env.PORT;
 
 //export const mongoDBURL =
  // 'mongodb+srv://root:root@books-store-mern.c7ba8z1.mongodb.net/books-collection?retryWrites=true&w=majority';
